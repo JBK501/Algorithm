65921fb2a6c745cd280331d5d52e204c7c8c8f4d
kanghyunhoo <eveneser@naver.com>
2022-12-23 21:06:40 +0900

    ADD Tree ADD PriorityQueue

--- Algorithm/Bfs.cpp
+++ Algorithm/Bfs.cpp
@@ -1,116 +1,116 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-struct vertex
-{
-	// int data;
-};
-
-vector<vertex> vertices;	// 정점 목록
-vector<vector<int>> adjacent;	// 인접 목록
-vector<bool> discovered; // 발견한 목록
-
-void creategraph()
-{
-	vertices.resize(6);
-	adjacent = vector<vector<int>>(6);
-
-
-	 //인접 리스트
-	/*adjacent[0].push_back(1);
-	adjacent[0].push_back(3);
-	adjacent[1].push_back(0);
-	adjacent[1].push_back(2);
-	adjacent[1].push_back(3);
-	adjacent[3].push_back(4);
-	adjacent[5].push_back(4);*/
-
-	// 인접 행렬
-	adjacent = vector<vector<int>>
-	{
-		{0,1,0,1,0,0},
-		{1,0,1,1,0,0},
-		{0,0,0,0,0,0},
-		{0,0,0,0,1,0},
-		{0,0,0,0,0,0},
-		{0,0,0,0,1,0},
-	};
-}
-
-// bfs(breadt first search) 너비 우선 탐색	--> 입구에서 가까운 정점을 기준으로 탐색한다.
-void Bfs(int here)
-{
-	// 누구에 의해 발견 되었는지?
-	vector<int> parent(6, -1);
-
-	// 시작점에서 얼만큼 떨어져 있는지?
-	vector<int> distance(6, -1);
-
-
-	queue<int> q;	// 발견한 정점목록을 관리하는 큐 생성 (예약시스템)
-	q.push(here);	// 발견한 정점 추가
-	discovered[here] = true;	// 발견여부 표시
-
-	parent[here] = here;
-	distance[here] = 0;
-
-	while (q.empty() == false)
-	{
-		here = q.front();
-		q.pop();
-
-		cout << "visited : " << here << endl;
-
-
-		// 인접 리스트 version
-		//for (int there : adjacent[here])
-		//{
-		//	if (discovered[there])
-		//		continue;
-
-		//	q.push(there);	// 발견한 정점 추가
-		//	discovered[there] = true;	// 발견여부 표시
-
-		//	parent[there] = here;
-		//	distance[there] = distance[here] + 1;
-		//}
-
-		// 인접 행렬 version
-		for (int there = 0 ; there < 6 ; there++)
-		{
-			if (adjacent[here][there] == 0)
-				continue;
-
-			if (discovered[there])
-				continue;
-
-			q.push(there);	// 발견한 정점 추가
-			discovered[there] = true;	// 발견여부 표시
-
-			parent[there] = here;
-			distance[there] = distance[here] + 1;
-		}
-	}
-}
-
-void Bfsall()
-{
-	for (int i = 0; i < 6; i++ )
-	{
-		if (discovered[i] == false)
-			Bfs(i);
-	}
-}
-
-//int main()
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//struct vertex
 //{
-//	creategraph();
+//	// int data;
+//};
 //
-//	discovered = vector<bool>(6, false);
+//vector<vertex> vertices;	// 정점 목록
+//vector<vector<int>> adjacent;	// 인접 목록
+//vector<bool> discovered; // 발견한 목록
 //
-//	Bfs(0);
-//	//Bfsall();
+//void creategraph()
+//{
+//	vertices.resize(6);
+//	adjacent = vector<vector<int>>(6);
+//
+//
+//	 //인접 리스트
+//	/*adjacent[0].push_back(1);
+//	adjacent[0].push_back(3);
+//	adjacent[1].push_back(0);
+//	adjacent[1].push_back(2);
+//	adjacent[1].push_back(3);
+//	adjacent[3].push_back(4);
+//	adjacent[5].push_back(4);*/
+//
+//	// 인접 행렬
+//	adjacent = vector<vector<int>>
+//	{
+//		{0,1,0,1,0,0},
+//		{1,0,1,1,0,0},
+//		{0,0,0,0,0,0},
+//		{0,0,0,0,1,0},
+//		{0,0,0,0,0,0},
+//		{0,0,0,0,1,0},
+//	};
 //}
+//
+//// bfs(breadt first search) 너비 우선 탐색	--> 입구에서 가까운 정점을 기준으로 탐색한다.
+//void Bfs(int here)
+//{
+//	// 누구에 의해 발견 되었는지?
+//	vector<int> parent(6, -1);
+//
+//	// 시작점에서 얼만큼 떨어져 있는지?
+//	vector<int> distance(6, -1);
+//
+//
+//	queue<int> q;	// 발견한 정점목록을 관리하는 큐 생성 (예약시스템)
+//	q.push(here);	// 발견한 정점 추가
+//	discovered[here] = true;	// 발견여부 표시
+//
+//	parent[here] = here;
+//	distance[here] = 0;
+//
+//	while (q.empty() == false)
+//	{
+//		here = q.front();
+//		q.pop();
+//
+//		cout << "visited : " << here << endl;
+//
+//
+//		// 인접 리스트 version
+//		//for (int there : adjacent[here])
+//		//{
+//		//	if (discovered[there])
+//		//		continue;
+//
+//		//	q.push(there);	// 발견한 정점 추가
+//		//	discovered[there] = true;	// 발견여부 표시
+//
+//		//	parent[there] = here;
+//		//	distance[there] = distance[here] + 1;
+//		//}
+//
+//		// 인접 행렬 version
+//		for (int there = 0 ; there < 6 ; there++)
+//		{
+//			if (adjacent[here][there] == 0)
+//				continue;
+//
+//			if (discovered[there])
+//				continue;
+//
+//			q.push(there);	// 발견한 정점 추가
+//			discovered[there] = true;	// 발견여부 표시
+//
+//			parent[there] = here;
+//			distance[there] = distance[here] + 1;
+//		}
+//	}
+//}
+//
+//void Bfsall()
+//{
+//	for (int i = 0; i < 6; i++ )
+//	{
+//		if (discovered[i] == false)
+//			Bfs(i);
+//	}
+//}
+//
+////int main()
+////{
+////	creategraph();
+////
+////	discovered = vector<bool>(6, false);
+////
+////	Bfs(0);
+////	//Bfsall();
+////}
---
+++ Algorithm/PriorityQueue.cpp
@@ -0,0 +1,116 @@
+//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+//// --- 힙 트리 ---
+//// [부모 노드]가 가진 값은 항상 [자식 노드]가 가진 값보다 크다.
+//// 마지막 레벨을 제외한 모든 레벨에 노드가 꽉 차 있다. (완전 이진 트리)
+//// 마지막 레벨에 노드가 있을 때는, 항상 왼쪽부터 순서대로 채워야 한다.
+//// 노드의 개수를 알면, 트리 구조는 무조건 확정할 수 있다. => vector로 표현 가능
+//// -> i번 노드의 왼쪽 자식은 [(2 * 1) + 1]번
+//// -> i번 노드의 오른쪽 자식은[(2 * 1) + 2]번
+//// -> i번 노드의 부모는 [(i - 1) / 2]번
+//
+//// 우선순위큐는 힙 트리구조 이다.
+//
+//template<typename T, typename Container = vector<T>, typename Predicate = less<T>>
+//class PriorityQueue
+//{
+//public:
+//	void push(const T& data)
+//	{
+//		// 우선 힙 구조부터 맞춰춘다.
+//		_heap.push_back(data);
+//
+//		// 도장깨기 시작
+//		int now = static_cast<int>(_heap.size()) - 1;
+//
+//		// 루트 노드까지
+//		while (now > 0)
+//		{
+//			// 부모 노드의 데이터와 비교해서 더 작으면 패배
+//			int next = (now - 1) / 2;
+//			/*if (_heap[now] < _heap[next])
+//				break;*/
+//			if (_predicate(_heap[now], _heap[next]))
+//				break;
+//
+//			// 데이터 교체
+//			::swap(_heap[now], _heap[next]);
+//			now = next;
+//		}
+//	}
+//	void pop()
+//	{
+//		_heap[0] = _heap.back();
+//		_heap.pop_back();
+//
+//		int now = 0;
+//
+//		while (true)
+//		{
+//			int left = 2 * now + 1;
+//			int right = 2 * now + 2;
+//
+//			// 리프에 도달한 경우
+//			if (left >= _heap.size())
+//				break;
+//
+//			int next = now;
+//
+//			// 왼쪽과 비교
+//			/*if (_heap[next] < _heap[left])
+//				next = left;*/
+//			if (_predicate(_heap[next], _heap[left]))
+//				next = left;
+//
+//			// 둘 중 승자를 오른쪽과 비교
+//			/*if (right < (int)_heap.size() && _heap[next] < _heap[right])
+//				next = right;*/
+//			if (right < (int)_heap.size() && _predicate(_heap[next], _heap[right]))
+//				next = right;
+//
+//			// 왼쪽/오른쪽 둘 다 현재 값보다 작으면 종료
+//			if (next == now)
+//				break;
+//
+//			::swap(_heap[now], _heap[next]);
+//
+//			now = next;
+//		}
+//	}
+//	T& top()
+//	{
+//		return _heap[0];
+//	}
+//	bool empty()
+//	{
+//		return _heap.empty();
+//	}
+//
+//private:
+//	Container _heap = {};
+//	Predicate _predicate = {};
+//};
+//
+//
+//
+//int main()
+//{
+//	PriorityQueue<int, vector<int>, greater<int>> pq;
+//
+//	pq.push(100);
+//	pq.push(200);
+//	pq.push(300);
+//	pq.push(500);
+//	pq.push(400);
+//
+//	while (pq.empty() == false)
+//	{
+//		int value = pq.top();
+//		pq.pop();
+//
+//		cout << value << endl;
+//	}
+//}
